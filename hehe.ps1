139918c0f4533dd09f6654a57631a72c23a1e9ac
Andreas Koch <47931272+Andreasgedsen@users.noreply.github.com>
2022-08-23 12:45:09 +0200

    Update hehe.ps1

--- hehe.ps1
+++ hehe.ps1
@@ -36,6 +36,6 @@ public class Params
  
 }
  
-$url = "https://www.nemlig.com/scommerce/images/ost-6.jpg?v=AP2DIubK&h=116.667&w=176.667&i=%7B5BE03944-C1E8-4FF5-B194-82FB6BC96A60%7D%22;Invoke-WebRequest $url -OutFile $env:USERPROFILE\downloads\ost.jpg
+$url = "https://www.nemlig.com/scommerce/images/ost-6.jpg?v=AP2DIubK&h=116.667&w=176.667&i=%7B5BE03944-C1E8-4FF5-B194-82FB6BC96A60%7D;Invoke-WebRequest $url -OutFile $env:USERPROFILE\downloads\ost.jpg
 
 Set-WallPaper -Image “$env:USERPROFILE\downloads\ost.jpg”
