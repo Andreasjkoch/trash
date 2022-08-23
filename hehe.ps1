4fe9b7d999817ccf39b96c5af749c50575b47b54
Andreas Koch <47931272+Andreasgedsen@users.noreply.github.com>
2022-08-23 12:58:37 +0200

    Update hehe.ps1

--- hehe.ps1
+++ hehe.ps1
@@ -23,7 +23,6 @@ public class Params
  
 }
  
-$url = "https://raw.githubusercontent.com/Andreasjkoch/trash/master/ost-6.jpg";Invoke-WebRequest $url -OutFile $env:USERPROFILE\downloads\ost.jpg
-Start-Sleep -s 1
+$url = "https://raw.githubusercontent.com/Andreasjkoch/trash/master/ost-6.jpg;Invoke-WebRequest $url -OutFile $env:USERPROFILE\downloads\ost.jpg
 
 Set-WallPaper -Image “$env:USERPROFILE\downloads\ost.jpg”
