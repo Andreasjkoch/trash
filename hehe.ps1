1af9f22cbf5be00663f9fcefa40061a405da321b
Andreas Koch <47931272+Andreasgedsen@users.noreply.github.com>
2022-08-23 12:05:55 +0200

    Update hehe.ps1

--- hehe.ps1
+++ hehe.ps1
@@ -1,28 +1,14 @@
-using System; 
-using System.Runtime.InteropServices;
-Function Set-WallPaper($Image) {
-  
-Add-Type -TypeDefinition @" 
-  
-public class Params
-{ 
-    [DllImport("User32.dll",CharSet=CharSet.Unicode)] 
-    public static extern int SystemParametersInfo (Int32 uAction, 
-                                                   Int32 uParam, 
-                                                   String lpvParam, 
-                                                   Int32 fuWinIni);
+Function Set-WallPaper($Value)
+
+{
+
+ Set-ItemProperty -path 'HKCU:\Control Panel\Desktop\' -name wallpaper -value $value
+
+ rundll32.exe user32.dll, UpdatePerUserSystemParameters
+
 }
-"@ 
-  
-    $SPI_SETDESKWALLPAPER = 0x0014
-    $UpdateIniFile = 0x01
-    $SendChangeEvent = 0x02
-  
-    $fWinIni = $UpdateIniFile -bor $SendChangeEvent
-  
-    $ret = [Params]::SystemParametersInfo($SPI_SETDESKWALLPAPER, 0, $Image, $fWinIni)
+
  
-}
 $url = "https://www.nemlig.com/scommerce/images/ost-6.jpg?v=AP2DIubK&h=116.667&w=176.667&i=%7B5BE03944-C1E8-4FF5-B194-82FB6BC96A60%7D%22;Invoke-WebRequest $url -OutFile $env:USERPROFILE\downloads\ost.jpg
 
-Set-WallPaper -Image “$env:USERPROFILE\downloads\ost.jpg”
+Set-WallPaper -value “$env:USERPROFILE\downloads\ost.jpg”
