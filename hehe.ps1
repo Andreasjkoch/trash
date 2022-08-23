f18ed023aeace1bfdebbe11d9198c15a84107c36
Andreas Koch <47931272+Andreasgedsen@users.noreply.github.com>
2022-08-23 12:29:37 +0200

    Update hehe.ps1

--- hehe.ps1
+++ hehe.ps1
@@ -9,6 +9,6 @@ Function Set-WallPaper($Value)
 }
 
  
-$url = "https://www.nemlig.com/scommerce/images/ost-6.jpg?v=AP2DIubK&h=116.667&w=176.667&i=%7B5BE03944-C1E8-4FF5-B194-82FB6BC96A60%7D%22;Invoke-WebRequest $url -OutFile $env:USERPROFILE\downloads\ost.jpg
+$url = "https://www.nemlig.com/scommerce/images/ost-6.jpg?v=AP2DIubK&h=116.667&w=176.667&i=%7B5BE03944-C1E8-4FF5-B194-82FB6BC96A60%7D%22";Invoke-WebRequest $url -OutFile $env:USERPROFILE\downloads\ost.jpg
 
 Set-WallPaper -value “$env:USERPROFILE\downloads\ost.jpg”
